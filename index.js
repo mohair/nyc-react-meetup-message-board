42861b6327ef5c91b150197438a38c10d6838a9a
Mohammed Al Rujayi <mohammed@wasphi.com>
2017-04-06 18:14:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,4 +38,7 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+//server.listen(3000)
+server.listen(process.env.PORT || 3000, () => {
+  console.log('listening on', server.address().port);
+});
