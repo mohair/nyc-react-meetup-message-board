00a1d73d6f6bd2beb7e78223f1a415fde6f30ee2
Mohammed Al Rujayi <mohammed@wasphi.com>
2017-04-07 17:23:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class HelloWorld extends React.Component {
-  render: fumction(){
+  render: function(){
     return (
     <div>
       Hello World!
