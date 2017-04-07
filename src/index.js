688359cdc197a4831f16a5f8fa4096b8469e4b30
Mohammed Al Rujayi <mohammed@wasphi.com>
2017-04-07 17:42:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class HelloWorld extends React.Component {
-  render: function(){
+
+let Hello = React.createClass({
+
+  getInitialState: function(){
+    return { selectedName: this.getNames()[0] };
+  },
+
+  getNames: function(){
+    return ["Batman", "Robin", "Alfred", "Joker", "Penguin"];
+  },
+
+  handleNameChange: function(name){
+    this.setState({ selectedName: name });
+  },
+
+  render: function() {
     return (
-    <div>
-      Hello World!
-    </div>
+      <div>
+        <NamePicker
+          names={this.getNames()}
+          onNameChange={this.handleNameChange} />
+
+        <br/> <br/>
+
+        Hello, {this.state.selectedName.toUpperCase()}!!
+      </div>
     );
   }
+});
 
-}
+let NamePicker = React.createClass({
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'))
+  propTypes: {
+    names: React.PropTypes.array.isRequired,
+    onNameChange: React.PropTypes.func.isRequired
+  },
+
+  onNameChange: function(event){
+    this.props.onNameChange(event.target.value);
+  },
+
+  render: function(){
+    return (
+      <select onChange={this.onNameChange}>
+        {this.props.names.map((name) => {
+          return <option value={name}>{name}</option>;
+        })}
+      </select>
+    );
+  }
+});
+
+ReactDOM.render(<Hello />, document.getElementById('root'))
